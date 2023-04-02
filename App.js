46351f1c83ee726cb59cdb8a35c4fe4d39d89c59
MangeshNamewar <namewar.mangesh@gmail.com>
2023-04-02 11:50:02 +0530

    Basic of JSX

--- App.js
+++ App.js
@@ -1,34 +1,17 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1> I'm h1 tag</h1>
- *          <h2> I'm h2 tag</h2>
- *      </div>
- *      <div id="child2">
- *          <h1> I'm h1 tag</h1>
- *          <h2> I'm h2 tag</h2>
- *      </div>
- * </div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
- import React from "react";
- import ReactDOM from "react-dom/client";
+// React Element => ReactElement- JS Object =>  HTMLElement(Render)
+const heading = React.createElement("h1", {id: "heading"}, "Namaste React..!!");
+console.log(heading);
 
-const parent = React.createElement('div', {id: 'parent'}, 
-    [React.createElement("div", {id: 'child'},[
-        React.createElement('h1',{},'This is Namaste React'),
-        React.createElement('h2',{},'Learn Here.')
-    ]),
-    React.createElement("div", {id: 'child2'},[
-        React.createElement('h1',{},'Im h1 tag.'),
-        React.createElement('h2',{},'Im h2 tag.')
-    ])]
-);
+// JSX - HTML-like or XML-like syntax
+// JSX => Babel transpiled it to React.createElement => ReactElement - JS Object => HTMLElement(render)
 
-// JSX
+const jsxHeading = <h1 id="heading" className="heading">Namaste React using JSX..!!</h1>;
+console.log(jsxHeading);
 
-console.log(parent); // object
+// JSX (transpiled before it reaches to the JS) -> PARCEL => BABEL
 
-//const heading = React.createElement('h1',{id:'heading'},"Hellow World from React..!!");
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(jsxHeading);
