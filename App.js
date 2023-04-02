7f762751790abcba192468403ecc5b0865398a39
MangeshNamewar <namewar.mangesh@gmail.com>
2023-04-02 09:02:28 +0530

    episode-02 Igniting App

--- App.js
+++ App.js
@@ -11,10 +11,13 @@
  * </div>
  */
 
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+
 const parent = React.createElement('div', {id: 'parent'}, 
     [React.createElement("div", {id: 'child'},[
-        React.createElement('h1',{},'Im h1 tag.'),
-        React.createElement('h2',{},'Im h2 tag.')
+        React.createElement('h1',{},'This is Namaste React'),
+        React.createElement('h2',{},'Learn Here.')
     ]),
     React.createElement("div", {id: 'child2'},[
         React.createElement('h1',{},'Im h1 tag.'),
